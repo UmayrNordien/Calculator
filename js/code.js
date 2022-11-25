47350193f665f7a981308b15d9916b433fd66413
UmayrNordien <umayrnordien@gmail.com>
2022-11-25 14:24:06 +0200

    updated comments

--- js/code.js
+++ js/code.js
@@ -1,40 +1,40 @@
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-      this.previousOperandTextElement = previousOperandTextElement
+    constructor(previousOperandTextElement, currentOperandTextElement) { //where to display the output display text for the cal
+      this.previousOperandTextElement = previousOperandTextElement //variables for this class,set it inside the calculator class
       this.currentOperandTextElement = currentOperandTextElement
-      this.clear()
+      this.clear()      //clear all inputs and reset to default values
     }
   
     clear() {
-      this.currentOperand = ''
+      this.currentOperand = ''          // '' replaces text with an empty string
       this.previousOperand = ''
       this.operation = undefined
     }
   
     delete() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1)
+      this.currentOperand = this.currentOperand.toString().slice(0, -1)         //clears range of numbers
     }
   
-    appendNumber(number) {
-      if (number === '.' && this.currentOperand.includes('.')) return
-      this.currentOperand = this.currentOperand.toString() + number.toString()
+    appendNumber(number) {          //click on a number, then displays on the output 
+      if (number === '.' && this.currentOperand.includes('.')) return       //stops function from executing further (adding a single period.)
+      this.currentOperand = this.currentOperand.toString() + number.toString()      //want numbers to be appended and not added (as strings)
     }
   
-    chooseOperation(operation) {
-      if (this.currentOperand === '') return
+    chooseOperation(operation) {        
+      if (this.currentOperand === '') return        // not let us execute any further
       if (this.previousOperand !== '') {
         this.compute()
       }
-      this.operation = operation
-      this.previousOperand = this.currentOperand
+      this.operation = operation        //set to operation so that the cal knows which operation to use when it computes the value
+      this.previousOperand = this.currentOperand        //done typing current number and recycled over to previous operand and clear out new current operand
       this.currentOperand = ''
     }
   //COMPUTATION
-    compute() {
-      let computation
-      const prev = parseFloat(this.previousOperand)
+    compute() {             //compute a single value for what we need to display on the cal
+      let computation           //result of compute function
+      const prev = parseFloat(this.previousOperand)         //number version of the previous operand (converting string to number)
       const current = parseFloat(this.currentOperand)
-      if (isNaN(prev) || isNaN(current)) return
+      if (isNaN(prev) || isNaN(current)) return         //user doesn't enter anything, we don't want the code to run (which cancels the function completely)
       switch (this.operation) {
         /* The CASE expression goes through conditions and returns a value 
         when the first condition is met (like an if-then-else statement). 
@@ -52,7 +52,7 @@ class Calculator {
         case '÷':
           computation = prev / current
           break
-        default:
+        default:        //else statement (incase none of these values get executed - invalid operation)
           return
       }
       this.currentOperand = computation
@@ -60,9 +60,9 @@ class Calculator {
       this.previousOperand = ''
     }
   
-    getDisplayNumber(number) {
+    getDisplayNumber(number) {      //update values inside the output
       const stringNumber = number.toString()
-      const integerDigits = parseFloat(stringNumber.split('.')[0])
+      const integerDigits = parseFloat(stringNumber.split('.')[0]) //decimal
       const decimalDigits = stringNumber.split('.')[1]
       let integerDisplay
       if (isNaN(integerDigits)) {
@@ -77,8 +77,8 @@ class Calculator {
       }
     }
   
-    updateDisplay() {
-      this.currentOperandTextElement.innerText =
+    updateDisplay() {           //update values inside the output
+      this.currentOperandTextElement.innerText =        
         this.getDisplayNumber(this.currentOperand)
       if (this.operation != null) {
         this.previousOperandTextElement.innerText =
@@ -90,16 +90,16 @@ class Calculator {
   }
   
   //VARIABLES
-  const numberButtons = document.querySelectorAll('[data-number]')
+  const numberButtons = document.querySelectorAll('[data-number]')  // querySelectorAll ,all elements that match a certain string, data number gets all number elements
   const operationButtons = document.querySelectorAll('[data-operation]')
-  const equalsButton = document.querySelector('[data-equals]')
+  const equalsButton = document.querySelector('[data-equals]')  //querySelector used just for one element
   const deleteButton = document.querySelector('[data-delete]')
   const allClearButton = document.querySelector('[data-all-clear]')
   const previousOperandTextElement = document.querySelector('[data-previous-operand]')
   const currentOperandTextElement = document.querySelector('[data-current-operand]')
   
-  //CALCULATOR
-  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  //CALCULATOR (making the variables above operate on the cal)
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)      //new defines classes
   //NUMBERS BUTTON
   numberButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -109,9 +109,9 @@ class Calculator {
   })
   //OPERATIONS BUTTON
   operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', () => {        //each one
       calculator.chooseOperation(button.innerText)
-      calculator.updateDisplay()
+      calculator.updateDisplay()        //every-time a number is clicked, the display is updated
     })
   })
   //EQUALS BUTTON
